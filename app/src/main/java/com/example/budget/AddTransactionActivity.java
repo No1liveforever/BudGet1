ef3205511b83a8233ce42703cfae67b57c300185
No1liveforever <begat651>
2023-01-29 00:40:28 +0100

    first version of BudGet

---
+++ app/src/main/java/com/example/budget/AddTransactionActivity.java
@@ -0,0 +1,106 @@
+package com.example.budget;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.budget.databinding.ActivityAddTransactionBinding;
+import com.example.budget.databinding.ActivityDashboardBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class AddTransactionActivity extends AppCompatActivity {
+    ActivityAddTransactionBinding binding;
+    FirebaseFirestore fStore;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    String type="";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityAddTransactionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        fStore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseUser=firebaseAuth.getCurrentUser();
+
+
+        binding.expenseCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                type = "Expense";
+                binding.expenseCheckBox.setChecked(true);
+                binding.incomeCheckBox.setChecked(false);
+            }
+        });
+
+        binding.incomeCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                type="Income";
+                binding.expenseCheckBox.setChecked(false);
+                binding.incomeCheckBox.setChecked(true);
+            }
+        });
+
+        binding.btnAddTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String amount = binding.userAmountAdd.getText().toString().trim();
+                String note = binding.userNoteAdd.getText().toString().trim();
+                if (amount.length() <= 0) {
+                    return;
+                }
+
+                if (type.length() <= 0) {
+                    Toast.makeText(AddTransactionActivity.this, "Select transaction type", Toast.LENGTH_SHORT).show();
+                }
+
+                String id = UUID.randomUUID().toString();
+                Map<String, Object> transaction = new HashMap<>();
+                transaction.put("id", id);
+                transaction.put("amount", amount);
+                transaction.put("note", note);
+                transaction.put("type", type);
+                transaction.put("date","23 july 2021");
+
+                fStore.collection("Expense").document(firebaseAuth.getUid()).collection("Notes").document(id)
+                        .set(transaction)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(AddTransactionActivity.this, "Added", Toast.LENGTH_SHORT).show();
+                                binding.userNoteAdd.setText("");
+                                binding.userAmountAdd.setText(" ");
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(AddTransactionActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                            }
+                        });
+
+
+            }
+        });
+
+    }
+}
